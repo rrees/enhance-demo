dde7a069c3698fa693d297b79214f99426fff11b
Robert Rees <shudderfix@gmail.com>
2023-05-10 14:24:51 +0100

    Creates a working element

---
+++ app/elements/site-header.mjs
@@ -0,0 +1,9 @@
+export default function SiteHeader({ html }) {
+  return html`<header>
+    <h1>Page title</h1>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </nav>
+</header>`;
+}  
